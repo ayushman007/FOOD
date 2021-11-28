6ce7a59b9b037b1539a9ad6a977c03bc4ad10cef
ayushman007 <ayushmanpaul007@gmail.com>
2021-11-28 15:09:06 +0530

    7

--- app/src/main/java/com/example/food/view/activities/AddUpdateDish.kt
+++ app/src/main/java/com/example/food/view/activities/AddUpdateDish.kt
@@ -3,10 +3,24 @@ package com.example.food.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.food.R
+import com.example.food.databinding.ActivityAddDishBinding
 
 class AddUpdateDish : AppCompatActivity() {
+    private lateinit var mBinding:ActivityAddDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_dish)
+        mBinding= ActivityAddDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+
+        setUpActionBar()
+    }
+    //implementing the backButton on toolbar
+    private  fun setUpActionBar(){
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
